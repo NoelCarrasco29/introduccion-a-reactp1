4daae673f9b6e22bf2c65f8fc71c28a23e8ba5ef
Noel Carrasco <carrascon3055@gmail.com>
2022-05-05 23:01:24 -0600

    agregamos el constructor para la clase

--- tictactoe/src/index.js
+++ tictactoe/src/index.js
@@ -23,9 +23,6 @@ class Board extends React.Component {
   }
   handleClick(i) {
     const squares = this.state.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       squares: squares,
@@ -43,13 +40,13 @@ class Board extends React.Component {
   }
 
   render() {
-    const winner = calculateWinner(this.state.squares);
+     const winner = calculateWinner(this.state.squares);
     let status;
     if (winner) {
       status = 'Winner: ' + winner;
     } else {
       status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-
+    }
     return (
       <div>
         <div className="status">{status}</div>
@@ -74,6 +71,15 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [{
+        squares: Array(9).fill(null),
+      }],
+      xIsNext: true,
+    };
+  }
   render() {
     return (
       <div className="game">
